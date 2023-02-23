7defd64168ecafc18a662e7510137d8b44437695
zeeshani26 <ilahiamaan606@gmail.com>
2023-02-23 22:27:25 +0530

    day3

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,52 +1,52 @@
-const express=require("express");
-const {UserModel}=require("../model/user.model");
+const express = require("express");
+const { UserModel } = require("../model/user.model");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
-const userrouter=express.Router();
+const userrouter = express.Router();
 
-userrouter.post("/register",async(req,res)=>{
-    let {name,email,password,gender,age,city}=req.body;
+userrouter.post("/register", async (req, res) => {
+    let { name, email, password } = req.body;
 
-    let abc=await UserModel.find({email:email});
+    let abc = await UserModel.find({ email: email });
 
-    if(abc.length>0){
-        res.send({"msg": "User already exist, please login"})
+    if (abc.length > 0) {
+        res.send({ "msg": "User already exist, please login" })
     }
-    else{
-        bcrypt.hash(password, 2,async function(err, hash) {
-            let user=new UserModel({name,email,password:hash,gender,age,city});
+    else {
+        bcrypt.hash(password, 2 , async function (err, hash) {
+            let user = new UserModel({ name, email, password: hash });
             await user.save();
-            res.send({"msg": "You are registered"})
+            res.send({ "msg": "You are registered" })
         });
     }
 })
 
 
 
-userrouter.post("/login",async(req,res)=>{
-    let {email,password}=req.body;
+userrouter.post("/login", async (req, res) => {
+    let { email, password } = req.body;
 
-    let abc=await UserModel.find({email:email});
+    let abc = await UserModel.find({ email: email });
 
-    if(abc.length>0){
-        bcrypt.compare(password, abc[0].password, function(err, result) {
-            if(result){
+    if (abc.length > 0) {
+        bcrypt.compare(password, abc[0].password, function (err, result) {
+            if (result) {
                 let token = jwt.sign({ userid: abc[0]._id }, 'shhhhh');
-                res.send({"msg": "Login Successful","token":token})
+                res.send({ "msg": "Login Successful", "token": token })
             }
-            else{
-                res.send({"msg": "Wrong Credentials"})
+            else {
+                res.send({ "msg": "Wrong Credentials" })
             }
         });
     }
-    else{
-        res.send({"msg": "Wrong Credentials"})
+    else {
+        res.send({ "msg": "Wrong Credentials" })
     }
 })
 
 
-module.exports={
+module.exports = {
     userrouter
 }
